908e902372d9df2b4656a61e629a3fa6dbf54dd6
Thinker-ljn <thinker_ljn@163.com>
2019-10-21 15:17:57 +0800

    fix package

--- rollup-build.js
+++ rollup-build.js
@@ -5,13 +5,16 @@ const rollupTypescript = require('rollup-plugin-typescript2');
 rollup.rollup({
   input: './src/index.ts',
   plugins: [
-    rollupTypescript(),
+    rollupTypescript({
+      clean: true,
+      exclude:['./test']
+    }),
     terser()
   ]
 }).then(bundle => {
   return bundle.write({
     file: './lib/rectangle-transform.js',
-    format: 'iife',
+    format: 'umd',
     name: 'RectangleTransform',
     exports: 'named',
     sourcemap: true
